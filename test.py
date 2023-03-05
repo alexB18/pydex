bdecb20975c287a6b293a67bcb4403cb8b7edc3a
alexB18 <trulyalexb@gmail.com>
2023-03-05 03:08:54 -0800

    pydex now displays the name/ID for a each pokemon as well as some flavor text. WARNING: this still doesn't fully work in the slightest! Will need to impliment a system that dynamically pulls the most recent, english flavor text for a given pokemon.

--- test.py
+++ test.py
@@ -2,8 +2,22 @@ if __name__ == "__main__":
     from ascii_magic import AsciiArt
     import pokebase as pb
 
+    # prompt user for pokemon ID number
     pokenumber = int(input('Enter pokemon number:\n'))
+    print("loading pokemon data...")
 
+    # load pokemon data into memory
+    pokemon = pb.pokemon(pokenumber)
+    
+    # display sprite and basic info
     sprite = pb.SpriteResource('pokemon', pokenumber)
     displayed_art = AsciiArt.from_url(sprite.url)
     displayed_art.to_terminal()
+    ## print basic info
+    print("Name: " + pokemon.name)
+    print("ID: " + str(pokenumber) + "\n")
+    
+    ## print flavor text
+    #TODO: make it so pydex looks for newest, *English* description in database.
+    #       > could be fun to give user the ability to choose preferred language
+    print(pb.pokemon_species(pokenumber).flavor_text_entries[15].flavor_text)
